8975f332c46e7797b549fb2626d68a7b8484427c
Pedro <priccis@gmail.com>
2018-11-26 21:46:10 -0300

    updates

---
+++ peer.py
@@ -0,0 +1,56 @@
+from p2p import *
+import threading
+
+class Peer(object):
+
+    def __init__(self, host, port, tracker_id):
+        self.my_host = host
+        self.my_port = port
+        self.my_id = f"{host}:{port}"
+        self.lock = threading.Lock()
+        self.running = True # State of peer
+        self.t_host, self.t_port = tracker_id.split(':')
+        self.t_port = int(self.t_port)
+
+    def connect_tracker(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.t_host, self.t_port))
+        tracker = P2PCommunication(s)
+        tracker.send(b"HELO test")
+        s.close()
+        return True
+
+    def run(self):
+        # First run
+        self.connect_tracker()
+        # Loop that listens for connections
+        sock = create_socket(self.my_host, self.my_port)
+
+        while self.running:
+            try:
+                # Wait for clients (tracker / other peer) to connect
+                cl_sock, cl_addr = sock.accept()
+                # When a client connects, create a thread and process it
+                # thrd = threading.Thread( target = self.CHANGE, args = [ ARGS ] )
+                # thrd.start()
+
+            except KeyboardInterrupt: # Ctrl + C to stop peer
+                self.running = False
+                print("Quitting...")
+                continue
+            except:
+                print("Error")
+                continue
+
+        sock.close()
+        print(f"Peer {self.my_id} says goodbye")
+
+
+
+
+
+
+
+if __name__=='__main__':
+    p1 = Peer('172.0.0.1', 9998, '172.0.0.1:9999')
+    p1.run()
---
+++ tracker.py
@@ -0,0 +1,102 @@
+from p2p import *
+import threading
+
+class Tracker(object):
+
+    def __init__(self, port):
+        self.my_port = port
+        self.peers = [] # List containing peer_id of each active peer
+        self.files = {} # filename : peer_id
+        self.lock = threading.Lock()
+        self.running = True # State of tracker
+        self.actions = {} # msg_id : action method
+    
+    def add_peer(self, peer_id):
+        self.peers.append(peer_id)
+    
+    def remove_peer(self, peer_id):
+        self.peers.remove(peer_id)
+    
+    def action_addf(self, client, msg_content):
+        ''' Add files sent by peer to the files dict '''
+        
+        pass
+    def action_helo(self, client, msg_content):
+        ''' Helo: Add me to the peers list '''
+        # If peer_id not in peers: append peer_id
+        # send acknowladge
+        # send error
+        pass
+
+    def action_rqfs(self, client, msg_content):
+        ''' Request file matching string ''' 
+        self.lock.acquire()
+        # Check if string in self.files
+        # If .. 
+        # else: client.send(ERRR)
+
+        self.lock.release()
+        pass
+    
+    def action_quit(self, client, msg_content):
+        ''' Quit: remove peer form self.peers ''' 
+        self.lock.acquire()
+        try:
+            # remove peer
+            # send 
+            pass
+        except:
+            # client.send(ERRR)
+            pass
+        finally:
+            self.lock.release()
+        pass
+
+    def process_client(self, client_socket):
+        host, port = client_socket.getpeername()
+        peer_id = f"{host}:{port}"
+        client = P2PCommunication(client_socket) # TODO
+        # Recieve message from client and redirect to corresponing action
+        msg_id, msg_content = client.recv()
+        print(msg_id, msg_content)
+        if msg_id in self.actions:
+            actions[msg_id](client, msg_content)
+        
+        client.close()
+
+    
+    def run(self):
+        sock = create_socket('', self.my_port)
+
+        while self.running:
+            try:
+                # Wait for clients to connect
+                cl_sock, cl_addr = sock.accept()
+                # When a client connects, create a thread and process it TODO
+                self.process_client(cl_sock)
+                # thrd = threading.Thread(target = self.process_client, args = [cl_sock] )
+                # thrd.start()
+
+            except KeyboardInterrupt: # Ctrl + C to stop tracker
+                self.running = False
+                print("Quitting...")
+                continue
+            except:
+                print("Error")
+                continue
+
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    t = Tracker(9999)
+    t.run()
