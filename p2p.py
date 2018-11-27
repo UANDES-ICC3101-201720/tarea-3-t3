4d468330afd869e595bdcfc74af13d7e89859a97
Pedro <priccis@gmail.com>
2018-11-26 21:45:41 -0300

    socket wrapper

---
+++ p2p.py
@@ -0,0 +1,12 @@
+
+class P2PCommunication(object):
+    ''' Socket based communication utility '''
+    def __init__(self, parameter_list):
+        pass
+
+    def send(self, parameter_list):
+        pass
+    
+    def recv(self, parameter_list):
+        pass
+    
