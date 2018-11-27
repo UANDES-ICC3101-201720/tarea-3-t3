5030473317a97d1a9b7036502a68d9d40abb7be1
Pedro <priccis@gmail.com>
2018-11-26 21:45:54 -0300

    more wrapping

--- p2p.py
+++ p2p.py
@@ -1,12 +1,34 @@
+import socket
 
 class P2PCommunication(object):
     ''' Socket based communication utility '''
-    def __init__(self, parameter_list):
-        pass
+    def __init__(self, socket):
+        if socket:
+            self.sock = socket
+        else:
+            # create socket? TODO
+            print("no socket")
+        
+        self.sd = self.sock.makefile( 'rw', 0 )
 
-    def send(self, parameter_list):
-        pass
+    def send(self, message):
+        self.sock.sendall(f"{message}") # TODO
+        print("Sent...")
+        return True
     
-    def recv(self, parameter_list):
-        pass
+    def recv(self):
+        msg_id = self.sd.read(4)
+        # Wile to read next bytes TODO
+        return (msg_id, 'content')
     
+    def close(self):
+        self.sock.close()
+        self.sd = None # garbage collector
+        return True
+
+def create_socket(host, port):
+        sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM ) # TCP/IP Socket
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To make port availible as soon as close()
+        sock.bind((host, port))
+        sock.listen(5)
+        return sock
